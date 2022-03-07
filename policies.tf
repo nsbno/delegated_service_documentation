c216ae06779f1c8f8ce072924685cadd8d54ba7b
arnhaa <arnehaagensen@gmail.com>
2022-03-07 11:01:39 +0100

    update policies

--- policies.tf
+++ policies.tf
@@ -113,27 +113,10 @@ data "aws_iam_policy_document" "s3_for_external_accounts" {
   }
 
   statement {
-    effect    = "Deny"
-    actions   = ["s3:PutObject", "s3:PutObjectAcl"]
-    resources = ["${aws_s3_bucket.staging.arn}/*"]
-    principals {
-      type        = "AWS"
-      identifiers = ["*"]
-    }
-    condition {
-      test     = "StringNotEquals"
-      variable = "s3:x-amz-acl"
-      values = [
-        "bucket-owner-full-control"
-      ]
-    }
-  }
-
-  statement {
     effect = "Allow"
     actions = [
       "s3:PutObject*",
-      "s3:DeleteObject*",
+      "s3:PutObjectAcl*",
       "s3:GetObject*"
     ]
     resources = formatlist("${aws_s3_bucket.staging.arn}/%s/$${aws:PrincipalAccount}/*", ["dev", "test", "stage", "prod"])
