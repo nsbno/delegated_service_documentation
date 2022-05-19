0fef5f383ce0518c9ad8f71b2225f45a3a169d20
arnhaa <arnehaagensen@gmail.com>
2022-05-19 10:39:58 +0200

    simplified lambda for edge auth

--- src/lambdaedge/lambdaedge.js
+++ src/lambdaedge/lambdaedge.js
@@ -12,8 +12,7 @@ async function asyncCalls(ssmname) {
   return data;
 }
 
-exports.handler = async function (event, context, callback) {
-  // Get request and request headers
+exports.handler = async  (event, context, callback) => {
   const request = event.Records[0].cf.request
   const headers = request.headers
 
@@ -21,25 +20,35 @@ exports.handler = async function (event, context, callback) {
   const authUser = await asyncCalls(SSMUser);
   const SSMPass = 'antoraportalpass'
   const authPass = await asyncCalls(SSMPass)
-  
-  
-    // Construct the Basic Auth string
-    const authString = 'Basic ' + new Buffer(authUser + ':' + authPass).toString('base64');
 
-    // Require Basic authentication
-    if (typeof headers.authorization == 'undefined' || headers.authorization[0].value != authString) {
-        const body = 'Unauthorized';
-        const response = {
-            status: '401',
-            statusDescription: 'Unauthorized',
-            body: body,
-            headers: {
-                'www-authenticate': [{key: 'WWW-Authenticate', value:'Basic'}]
-            },
-        };
-        callback(null, response);
+  const encodedCredentials = new Buffer(`${authUser}:${authPass}`).toString('base64')
+  const authString = `Basic ${encodedCredentials}`
+
+  console.log('event dump ', event);  
+  
+  if (
+    typeof headers.authorization == 'undefined' ||
+    headers.authorization[0].value != authString
+  ) {
+    const response = {
+      status: '401',
+      statusDescription: 'Unauthorized',
+      body: 'Unauthorized',
+      headers: {
+        'www-authenticate': [
+          {
+            key: 'WWW-Authenticate',
+            value: 'Basic',
+          }
+        ]
+      },
     }
+    console.log("failed")
+    callback(null, response)
+    return
+  }
+  console.log("Completed")
 
-    // Continue request processing if authentication passed
-    callback(null, request);
-};
+  // Continue request processing if authentication passed
+  callback(null, request)
+}
